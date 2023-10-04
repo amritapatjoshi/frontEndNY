1d25787780b023276413feee50b1c033af230279
Chaithra Muralidhar <chaithra.muralidhar@blueconchtech.com>
2023-10-04 07:53:51 +0530

    comments resolved

--- shared/components/hs-LottiePlayer/HS-LottiePlayer.tsx
+++ shared/components/hs-LottiePlayer/HS-LottiePlayer.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Lottie from 'react-lottie-player';
 interface LottiePlayerProps {
     animationData: any;
+    // default value of loop and play is false
     loop: boolean;
     play: boolean;
 }
 function LottiePlayer({ animationData, loop, play }: LottiePlayerProps) {
     return (
         <Lottie
-            loop={loop}
             animationData={animationData}
-            play={play}
+            loop={false}
+            play={false}
         />
     );
 }
