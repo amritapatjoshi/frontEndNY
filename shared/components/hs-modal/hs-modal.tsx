ff5d87e75a057da2eb8ef62cb60b4e7fda2c7ab4
Hardik Vipulwar <Hardik.Vipulwar@blueconchtech.com>
2023-10-09 22:48:40 +0530

    'feature/PREMVP525-ModalComponent'

---
+++ shared/components/hs-modal/hs-modal.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+import Box from "@mui/material/Box"
+import Modal from "@mui/material/Modal"
+
+interface IProps {
+    width?: string;
+    height?: string;
+    children?: React.ReactNode;
+    handleClose?: () => void;
+    open : boolean;
+}
+
+export default function HSModalComponent(props: IProps) {
+  const style = {
+    position: "fixed" as const,
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: props.width || "50%",
+    height: props.height || "50%",
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+    borderRadius:"2rem"
+  }
+
+  return (
+    <>
+      <Modal open={props.open ? true:false }
+        onClose={props.handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {props.children}
+        </Box>
+      </Modal>
+    </>
+  )
+}
