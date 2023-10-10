30297f628e2de566f133abdc4212e2a2e769eb56
HimaniH10 <himaniharkawat@gmail.com>
2023-10-10 16:49:26 +0530

    Added env and next configuration for caching images

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 
+const imagesHost = process.env.IMAGE_HOST_NAME || 'localhost';
+const imagesCacheTTL = process.env.IMAGE_CACHE_TTL || 60;
+
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "",
-  //       port: '',
-  //     },
-  //   ],
-  // },
+  images: {
+    minimumCacheTTL: +imagesCacheTTL,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: imagesHost
+      }
+    ]
+  }
 }
 
 const env = process.env.NEXT_PUBLIC_APP_ENV || 'local';
